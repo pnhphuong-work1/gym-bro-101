1222390d27d8125ed34d0ae9b2823b37978a9194
phatnh13 <phatnguyenhoang69@gmail.com>
2024-10-11 20:23:07 +0700

    <update> layout homepage <add> Footer <add> Header

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -2,12 +2,22 @@
 import React from 'react';
 import {useGlobalContext} from "@/context/GlobalContext";
 
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+import HomePageImage from "@/public/asset/home-page.png"
+import Image from "next/image";
+
 const HomePage = () => {
     const {fullName} = useGlobalContext();
     console.log(fullName)
     return (
-        <div>
-            <h1>Home Page {fullName}</h1>
+        <div className='flex flex-col h-lvh'>
+            <Header />
+            <Image
+                className='w-full h-[80%]'
+                src={HomePageImage}
+                alt='' />
+            <Footer/>
         </div>
     );
 };
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({children} : {children: React.ReactNode}) => {
     return (
-        <main className="bg-blue-300 min-h-screen w-full items-center justify-center">
+        <main className="bg-blue-300 h-lvh w-full items-center justify-center">
             {children}
         </main>
     );
