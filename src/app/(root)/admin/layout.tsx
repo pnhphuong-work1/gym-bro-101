969b873e162e87a3266ec8d3c0438a56759e739e
lqviet45 <lqviet455@gmail.com>
2024-10-14 16:51:06 +0700

    adding logout

--- src/app/(root)/admin/layout.tsx
+++ src/app/(root)/admin/layout.tsx
@@ -3,18 +3,19 @@ import React, {useEffect} from 'react';
 import {useGlobalContext} from "@/context/GlobalContext";
 import {useRouter} from "next/navigation";
 import {LoadingSpinner} from "@/components/shared/LoadingSpinner";
+import {Role} from "@/constants";
 
 const AdminLayout = ({children} : {children: React.ReactNode}) => {
     const router = useRouter();
     const {role, loading} = useGlobalContext();
 
     useEffect(() => {
-        if (role !== 'Admin' && !loading) {
+        if (role !== Role.Admin && !loading) {
             router.push('/login');  // Only perform this redirection on the client side
         }
     }, [role, router, loading]);  // Trigger this effect when `role` or `router` changes
 
-    if (role !== 'Admin') {
+    if (role !== Role.Admin) {
         return (
             <div className="flex items-center justify-center h-screen">
                 <LoadingSpinner
